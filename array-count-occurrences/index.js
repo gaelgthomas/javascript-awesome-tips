8c1dd223cd454ac58bbde8ff36b7e4b92320e3f9
Gaël Thomas <gael.thomas@outlook.fr>
2020-10-02 23:17:05 +0200

    Improve "array-count-occurences" code

--- array-count-occurrences/index.js
+++ array-count-occurrences/index.js
@@ -1,11 +1,10 @@
 const animals = ['owl', 'frog', 'canary', 'duck', 'duck', 'goose', 'owl']
 
-const animalsOccurenceInArray = [... new Set(animals)].map(elem => {
-    return {
-        key: elem,
-        value: animals.filter(animal => animal === elem).length
-    }
-})
+const animalsOccurenceInArray = [...new Set(animals)].map((elem) => ({
+    key: elem,
+    value: animals.filter((animal) => animal === elem).length,
+}))
+
 /* Output: [
     { key: 'owl', value: 2 },
     { key: 'frog', value: 1 },
