152b40bdb112c7f1e71656e38d3cb6314437ab39
Gaël Thomas <gael.thomas@outlook.fr>
2020-10-08 19:54:54 +0200

    Add a new tip

---
+++ string-to-number/index.js
@@ -0,0 +1,14 @@
+const myStringNumber = '1000'
+
+const myNumberWithUnaryOperator = +myStringNumber
+const myNumberWithNumberObject = Number(myStringNumber)
+const myNumberByMultiplying = myStringNumber * 1
+
+console.log(myNumberWithUnaryOperator)
+// Output: 1000
+
+console.log(myNumberWithNumberObject)
+// Output: 1000
+
+console.log(myNumberByMultiplying)
+// Output: 1000
