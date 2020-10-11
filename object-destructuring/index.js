41c2ef4736f7a8e064258fbd564d4680e992ad8e
Gaël Thomas <gael.thomas@outlook.fr>
2020-10-11 17:59:55 +0200

    Add a new tip

---
+++ object-destructuring/index.js
@@ -0,0 +1,15 @@
+const user = {
+    pseudo: 'Robotbotbot',
+    password: `Imarobot`,
+    age: '10000',
+}
+
+const { pseudo, age } = user
+
+console.log(pseudo, age)
+// Output: 'Robotbotbot', '10000'
+
+const { password } = user
+
+console.log(password)
+// Output: 'Imarobot'
